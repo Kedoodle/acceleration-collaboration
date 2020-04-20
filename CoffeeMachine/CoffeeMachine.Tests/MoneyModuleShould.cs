b1246f416dba530602e10849c04e4ccf5c1cc20d
Nhan Dang <nhantdang@gmail.com>
2020-04-20 10:50:48 +1000

    refactored moneymodule tests to receive inline data

--- CoffeeMachine/CoffeeMachine.Tests/MoneyModuleShould.cs
+++ CoffeeMachine/CoffeeMachine.Tests/MoneyModuleShould.cs
@@ -5,17 +5,23 @@ namespace CoffeeMachine.Test
     public class MoneyModuleShould
     {
 
-        [Fact]
-        public void DetermineEnoughMoneyForTea()
+        [Theory]
+        [InlineData(DrinkType.Coffee, 0.6)]
+        [InlineData(DrinkType.Tea, 0.4)]
+        [InlineData(DrinkType.HotChocolate, 0.5)]
+        public void DetermineEnoughMoneyForDrinkOrder(DrinkType drinkOrder, decimal amountPaid)
         {
-            var moneyModule = new MoneyModule {DrinkOrder = DrinkType.Tea, AmountPaid = 0.4m};
+            var moneyModule = new MoneyModule {DrinkOrder = drinkOrder, AmountPaid = amountPaid};
             Assert.True(moneyModule.IsOrderPaid());
         }
 
-        [Fact]
-        public void DetermineNotEnoughMoneyForTea()
+        [Theory]
+        [InlineData(DrinkType.Coffee, 0.5)]
+        [InlineData(DrinkType.Tea, 0.2)]
+        [InlineData(DrinkType.HotChocolate, 0.4)]
+        public void DetermineNotEnoughMoneyForDrinkOrder(DrinkType drinkOrder, decimal amountPaid)
         {
-            var moneyModule = new MoneyModule {DrinkOrder = DrinkType.Tea, AmountPaid = 0.2m};
+            var moneyModule = new MoneyModule {DrinkOrder = drinkOrder, AmountPaid = amountPaid};
             Assert.False(moneyModule.IsOrderPaid());
         }
         
